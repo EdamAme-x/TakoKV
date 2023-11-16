c36f5fe1e18ebda427a35d954ceb8b9a41fa6359
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2023-11-16 02:46:19 +0000

    add createTable | type

--- mod.d.ts
+++ mod.d.ts
@@ -1,12 +1,10 @@
 export interface KValue {
-  key: string[],
-  value: any,
-  versionstamp: number
+  key: string[];
+  value: any;
+  versionstamp: number;
 }
 
-export interface DB {
-  [key: string]: {
-    [key: string]: any[] | number, // column & row
-    id: number // id
-  } // table
-}
+export type DB = Record<string, {
+  [key: string]: any[] | number; // column & row
+  id: number; // id
+} | {}>; // tablet
--- mod.ts
+++ mod.ts
@@ -1,37 +1,44 @@
 import * as lodash from "https://deno.land/x/lodash@4.17.19/dist/lodash.min.js";
-import { KValue, DB } from "./mod.d.ts";
+import { DB, KValue } from "./mod.d.ts";
 
 export class TakoKV {
   constructor(
     public KV: any & {
-      set: Function,
-      get: Function
-    } // Temp Type
+      set: Function;
+      get: Function;
+    }, // Temp Type
   ) {
-    const init: KValue = this.KV.get(["_takokv_init"])
+    const init: KValue = this.KV.get(["_takokv_init"]);
     if (!init.value) {
-      this.KV.set(["_takokv_init"], true)
-      this.KV.set(["_takokv_db"], {}) // tables
+      this.KV.set(["_takokv_init"], true);
+      this.KV.set(["_takokv_db"], {}); // tables
     }
 
-    this.before = this.KV.get(["_takokv_db"])
-    this.current = Object.create(this.before)
+    this.before = this.KV.get(["_takokv_db"]);
+    this.current = Object.create(this.before);
   }
 
   before: DB = {}; // before database
   current: DB = {}; // current databse
 
+  /** @Methods **/
+  createTable(tableName: string): boolean {
+    if (!this.current[tableName]) {
+      this.current[tableName] = {};
+      return true;
+    }
+    return false;
+  }
+  
   update(): boolean {
-
     if (lodash.isEqual(this.before, this.current)) {
       return false;
     } // Non Update
 
-    // Update Start
-    this.KV.set(["_takokv_last"], Date.now()); // last update
-    // Update End
-    
-    this.before = this.current; // Executed
+    this.KV.set(["_takokv_db"], this.current); // update database
+    this.KV.set(["_takokv_last"], Date.now()); // last update time
+
+    this.before = Object.create(this.current); // Executed
     return true;
   }
 }
